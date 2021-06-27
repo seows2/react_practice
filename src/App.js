703956b2567c57b6353d3565a569a424ebdbe901
seows2 <seows2@naver.com>
2021-06-27 17:33:59 +0900

    useRef를 통한 키 값 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import Counter from './Counter';
+import CreateUser from './CreateUser';
 import Hello from './Hello';
 import InputSample from './InputSample';
+import UserList from './UserList';
 import Wrapper from './Wrapper';
 
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+
+  const { username, email } = inputs;
+  const onChange = ({ target }) => {
+    const { name, value } = target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    })
+  }
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    }
+    setUsers([...users, user])
+    setInputs({
+      username: "",
+      email: "",
+    });
+
+    nextId.current += 1;
+  }
+  
   return (
     <>
     <Wrapper>
@@ -14,6 +66,13 @@ function App() {
     </Wrapper>
     <Counter />
     <InputSample />
+    <CreateUser 
+      username={username}
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+    />
+    <UserList users={users}/>
    </>
   );
 }
---
+++ src/UserList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function User({user}) {
+    return (
+        <div>
+            <b>{user.username}</b> <span>({user.email})</span>
+        </div>
+    )
+}
+
+function UserList({users}) {
+    return (
+        <div>
+           {users.map((user, idx) => <User key={idx} user={user} />)}
+        </div>
+    )
+}
+
+export default UserList;
