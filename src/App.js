e50b4c620097c327d998f145759d28e6a5d030b2
seows2 <seows2@naver.com>
2021-08-03 17:46:46 +0900

    Prettierrc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useReducer, useRef } from 'react';
+import React, { useMemo, useReducer } from 'react';
 import './App.css';
 import Counter from './Counter';
 import CreateUser from './CreateUser';
@@ -6,6 +6,7 @@ import Hello from './Hello';
 import InputSample from './InputSample';
 import UserList from './UserList';
 import Wrapper from './Wrapper';
+import produce from "immer";
 
 function countActiveUsers(users) {
   console.log("활성 사용자는 세는 중...");
@@ -46,21 +47,19 @@ function reducer(state, action) {
         }
       };
     case "CREATE_USER":
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user)
+      })
     case "TOGGLE_USER":
-      return {
-        ...state,
-        users: state.users.map(user => 
-          user.id === action.id ? {...user, active: !user.active}: user)
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      })
     case "REMOVE_USER":
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       return state;
   }
@@ -74,7 +73,7 @@ function App() {
   const { users } = state;
 
   
-  const count = useMemo(() => countActiveUsers(users, [users]));
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
   <userDispatch.Provider value={dispatch}>
--- src/UserList.js
+++ src/UserList.js
@@ -1,35 +1,42 @@
-import React, { useContext } from 'react'
-import { userDispatch } from './App'
+import React, { useContext } from 'react';
+import { userDispatch } from './App';
 
 const User = React.memo(function User({ user }) {
-    const dispatch = useContext(userDispatch);
-    return (
-        <div>
-            <b style={{
-                cursor: "pointer",
-                color: user.active ? "green" : "black"
-            }}
-            onClick={() => {
-                dispatch({ type: "TOGGLE_USER", id: user.id })
-            }}
-            >
-                {user.username}
-            </b>
-            &nbsp;
-            <span>({user.email})</span>
-            <button onClick={() => {
-                dispatch({ type: "REMOVE_USER", id: user.id })
-            }}>삭제</button>
-        </div>
-    ) 
-})
+  const dispatch = useContext(userDispatch);
+  return (
+    <div>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black',
+        }}
+        onClick={() => {
+          dispatch({ type: 'TOGGLE_USER', id: user.id });
+        }}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button
+        onClick={() => {
+          dispatch({ type: 'REMOVE_USER', id: user.id });
+        }}
+      >
+        삭제
+      </button>
+    </div>
+  );
+});
 
 function UserList({ users }) {
-    return (
-        <div>
-           {users.map(user => <User key={user.id} user={user} />)}
-        </div>
-    )
+  return (
+    <div>
+      {users.map((user) => (
+        <User key={user.id} user={user} />
+      ))}
+    </div>
+  );
 }
 
 export default React.memo(UserList);
